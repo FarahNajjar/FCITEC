456f8c7acd9b9bd311731bc787aaa8b4e39ec54b
Rawan_Alshehri <rbinabduallah@stu.kau.edu.sa>
2023-11-21 12:08:58 +0300

    Rawan Add class user

---
+++ src/fcitec/User.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fcitec;
+
+/**
+ *
+ * @author Dell
+ */
+public class User {
+    private String name;
+    private String phoneNumber;
+    private int id;
+
+    // Constructor
+    public User(String name, String phoneNumber, int id) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.id = id;
+    }
+    // Setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+   // Method to request a report
+    public String requestReport() {
+        // You can customize the report generation logic here
+        return "Report requested by: " + name;
+    }
+}
+
