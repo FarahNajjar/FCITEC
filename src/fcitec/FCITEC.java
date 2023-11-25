b4f236d7fc3302a4d0401613adee560ba00e61ab
FARAH <FARAH@DESKTOP-JUAKP9J>
2023-11-25 17:51:38 +0300

    fix

--- src/fcitec/FCITEC.java
+++ src/fcitec/FCITEC.java
@@ -2,6 +2,7 @@ package fcitec;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -12,13 +13,33 @@ public class FCITEC {
     public static void main(String[] args) {
 
         int choice = 0;
-
+        int ID = 0;
         Scanner in = new Scanner(System.in);
 
-         // header
-        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-FCIT Emergency Center=-=-=-=-=-=-=-=-=-=-=-=-=-");
-        System.out.println("What would you like to do?");
-        System.out.println("1: File a new report \n2: view reports \n3: delete a report \n4: change a report's status(Admin Only) \n5: exit");
+        System.out.println("please enter your ID: ");
+        ID = in.nextInt();
+        String userIDString = Integer.toString(ID);
+//        String regex = "^0[0-9]*$";
+
+        System.out.println(userIDString.charAt(0));
+        if (userIDString.startsWith("0")) {
+            System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-FCIT Emergency Center(Admin)=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            System.out.println("What would you like to do?");
+            System.out.println("1: change a report's status(Admin Only)\n");
+        } else {
+            System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-FCIT Emergency Center(Admin)=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            System.out.println("What would you like to do?");
+        System.out.println("1: delete a report");
+
+            System.out.println("add a report");
+        }
+        System.out.println("2: view reports");
+        System.out.println("3: exit");
+        
+        // header
+//        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-FCIT Emergency Center=-=-=-=-=-=-=-=-=-=-=-=-=-");
+//        System.out.println("What would you like to do?");
+//        System.out.println("1: File a new report \n2: view reports \n3: delete a report \n4: change a report's status(Admin Only) \n5: exit");
 
         while (choice != 5) {
 
@@ -28,16 +49,16 @@ public class FCITEC {
                     Report.AddReport();
                     break;
                 case 2:
+                    //
                     Report.displayReports(ID);
                     break;
                 case 3:
+
                     Student.DeleteReport();
                     break;
                 case 4:
-                    System.out.println("please enter your ID: ");
-                    int ID = in.nextInt();
 
-                    String userIDString = String.valueOf(ID);
+                    //String userIDString = String.valueOf(ID);
                     if (userIDString.charAt(0) == '0') {
                         System.out.println("Welcome admin" + ID);
                         // change status
@@ -45,7 +66,7 @@ public class FCITEC {
 
                         System.out.println("unauthorized Access, admins only.");
                     }
-                    
+
                     break;
                 case 5:
                     System.out.println("Thank you for using the system, have a good day!");
