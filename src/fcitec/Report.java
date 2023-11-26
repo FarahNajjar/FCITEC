6322ef27817f3ca6ec175a9d0ea7f0b5ed173363
Rawan_Alshehri <rbinabduallah@stu.kau.edu.sa>
2023-11-26 20:19:40 +0300

    Merge origin/master
    Conflicts:
    	src/fcitec/FCITEC.java
    	src/fcitec/Report.java

--- src/fcitec/Report.java
+++ src/fcitec/Report.java
@@ -1,181 +1,183 @@
 package fcitec;
 
+ import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 class Report {
 
+  
+
     private static ArrayList<Report> reports = new ArrayList<>();
     private static int IDCounter = 1;
 
-    private int reportNumber;
-    private String location;
-    private String description;
-    private String reportStatus;
-    private String studentName;
-    private int phoneNumber;
-    private String statusDescription;
+    private int ReportNumber;
+    private String Location;
+    private String Description;
+    private String Status;
+    private User user;
 
-    public Report(int reportNumber, String location, String description, String reportStatus) {
-        this.reportNumber = reportNumber;
-        this.location = location;
-        this.description = description;
-        this.reportStatus = reportStatus;
+    public Report(int ReportNumber, String Location, String Description, User user, String Status) {
+        this.ReportNumber = ReportNumber;
+        this.Location = Location;
+        this.Description = Description;
+        this.user = user;
+        this.Status = Status;
     }
 
-    public Report(String location, String studentName, int phoneNumber, String statusDescription) {
-        this.location = location;
-        this.studentName = studentName;
-        this.phoneNumber = phoneNumber;
-        this.statusDescription = statusDescription;
+    public static int getIDCounter() {
+        return IDCounter;
     }
 
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getStatusDescription() {
-        return statusDescription;
-    }
-
-    public void setStatusDescription(String statusDescription) {
-        this.statusDescription = statusDescription;
+    public static void setIDCounter(int IDCounter) {
+        Report.IDCounter = IDCounter;
     }
 
     public int getReportNumber() {
-        return reportNumber;
+        return ReportNumber;
     }
 
-    public void setReportNumber(int reportNumber) {
-        this.reportNumber = reportNumber;
+    public void setReportNumber(int ReportNumber) {
+        this.ReportNumber = ReportNumber;
     }
 
     public String getLocation() {
-        return location;
+        return Location;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
+    public void setLocation(String Location) {
+        this.Location = Location;
     }
 
     public String getDescription() {
-        return description;
+        return Description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setDescription(String Description) {
+        this.Description = Description;
     }
 
-    public String getReportStatus() {
-        return reportStatus;
+    public String getStatus() {
+        return Status;
     }
 
-    public void setReportStatus(String reportStatus) {
-        this.reportStatus = reportStatus;
+    public void setStatus(String Status) {
+        this.Status = Status;
     }
 
-    public static ArrayList<Report> getReports() {
-        return reports;
+    public User getUser() {
+        return user;
     }
 
-    public static void addReport(Report report) {
-        reports.add(report);
+    public void setUser(User user) {
+        this.user = user;
     }
 
-
-    public static void AddReport() {
+    public static void ReportInfo(User user) {
         Scanner input = new Scanner(System.in);
-
-        System.out.print("Student Name: ");
-        String studentName = input.next();
-
-        System.out.print("Phone Number: ");
-        int phoneNumber = input.nextInt();
 
         System.out.print("Location: ");
         String location = input.next();
+        System.out.print("Description: ");
+        String description = input.next();
 
-        System.out.print("Status Description: ");
-        String statusDescription = input.next();
-
-        Report info = new Report(IDCounter, location, "PANIC ATTACK", "new");
+        Report info = new Report(IDCounter, location, description, user, "new");
         reports.add(info);
+        AddReport(info);
 
         IDCounter++; // Increment IDCounter
 
         System.out.println("\nReport has been added.");
     }
 
+    private static void AddReport(Report report) {
+        try {
+            FCITEC.myWriter = new FileWriter("Reports.txt");
 
+            String UserInfo = "Name: " + report.getUser().getName()
+                    + "\nPhone Number: " + report.getUser().getPhoneNumber()
+                    + "\nID: " + report.getUser().getId()
+                    + "\nReport Number: " + report.getReportNumber()
+                    + "\nLocation: " + report.getLocation()
+                    + "\nDescription: " + report.getDescription()
+                    + "\nReport Status: " + report.getStatus();
+
+            FCITEC.myWriter.write(UserInfo);
+            FCITEC.myWriter.write("\n----------------------------------------------------------------\n");
+            System.out.println(UserInfo);
+
+            FCITEC.myWriter.close();
+            System.out.println("\nNew report added successfully.");
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
 
     public static void displayReports(int userID) {
         System.out.println("------------------------- Report Details -------------------------");
-
         String userIDString = String.valueOf(userID);
-
-        if (userIDString.charAt(0) == '0') {
-            // Print all reports if the first character of the userIDString is '0' (ADMIN)
-            for (Report report : Report.getReports()) {
-                System.out.println("Report Number: " + report.getReportNumber()
-                        + "\nLocation: " + report.getLocation()
-                        + "\nDescription: " + report.getDescription()
-                        + "\nReport Status: " + report.getReportStatus());
-                System.out.println("\n----------------------------------------------------------------\n");
+        // calling check isAdmin method
+        if (userIDString.charAt(0) == '9') {        // Print all reports if the first character of the userIDString is '9' (ADMIN)
+            for (int i = 0; i < reports.size(); i++) {
+                Report loopReport = reports.get(i);
+                printReportDetails(loopReport);
             }
         } else {
-            // Print only the reports corresponding to the provided userID (Student)
-            for (Report report : Report.getReports()) {
-                if (report.getReportNumber() == userID) {
-                    System.out.println("Report Number: " + report.getReportNumber()
-                            + "\nLocation: " + report.getLocation()
-                            + "\nDescription: " + report.getDescription()
-                            + "\nReport Status: " + report.getReportStatus());
-                    System.out.println("\n----------------------------------------------------------------\n");
+            for (int i = 0; i < reports.size(); i++) {
+                Report loopReport = reports.get(i);
+                if (loopReport.getUser().getId() == userID) {
+                    printReportDetails(loopReport);
                 }
             }
         }
     }
 
-   public static void deleteReport(ArrayList<Report> reports, int reportNumber) {
-        boolean found = false;
+    private static void printReportDetails(Report loopReport) {
+        System.out.println("name: " + loopReport.getUser().getName()
+                + "\nPhone Number: " + loopReport.getUser().getPhoneNumber() + "\nID: " + loopReport.getUser().getId() + "Report Number: " + loopReport.getReportNumber()
+                + "\nLocation: " + loopReport.getLocation() + "\nDescription: " + loopReport.getDescription()
+                + "\nReport Status: " + loopReport.getStatus());
+        System.out.println("\n----------------------------------------------------------------\n");
+    }
 
-        for (Report report : reports) {
-            if (report.getReportNumber() == reportNumber) {
-                reports.remove(report);
-                System.out.println("Report with ID " + reportNumber + " has been deleted.");
-                found = true;
-                break;
+    public static Report searchByReportID(ArrayList<Report> ReportL, int ID) {
+        Report report = null;
+        for (Report R : ReportL) {
+            if (R.getReportNumber() == ID) {
+                report = R;
             }
         }
+        return report;
+    }
 
-        if (!found) {
-            System.out.println("Report with ID " + reportNumber + " not found.");
+     ///delete 
+    public static void deleteReport(int reportNumberToDelete) {
+        Report reportToDelete = searchByReportID(reports, reportNumberToDelete);
+        if (reportToDelete != null) {
+            reports.remove(reportToDelete);
+            updateFile(); // Update the file after removing the report
+            System.out.println("Report deleted successfully.");
+        } else {
+            System.out.println("Report not found.");
         }
     }
-    
 
+    private static void updateFile() {
+        try {
+            FileWriter myWriter = new FileWriter("Reports.txt");
 
-    // Method to delete a report from the array list 
-    public static void deleteReport(ArrayList<Report> reoprt,int reportNumber) {
-        for (Report R : reports) {
-            if (R.getReportNumber() == reportNumber) {
-                reoprt.remove(R);
-                System.out.println("Report with ID " + reportNumber + " has been deleted.");
-            }else {
-        System.out.println("Report with ID " + reportNumber  + " not found.");
-            } 
+            myWriter.write(" report successfully deleted ." );
+            myWriter.close();
+           
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
+    
+    
+
+    
