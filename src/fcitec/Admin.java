6f88f324b36de386e2767b4d223f9a0ae4f7b062
shahad Hamad Almasabi <sabdullahalmasabi0001@stu.kau.edu.sa>
2023-11-27 15:28:32 +0300

    change status method by shahad

--- src/fcitec/Admin.java
+++ src/fcitec/Admin.java
@@ -17,31 +17,5 @@ public class Admin extends User {
         super(AdminName, PhoneNum, ID);
     }
     
-       // Method to change the status of a report based on user input
-    public static void changeStatus(ArrayList<Report> reports, int reportNumber, int buttonChoice) {
-        // Step 1: Search for the report by report number
-        Report reportToChange = Report.searchByReportID(reports, reportNumber);
-
-        // Step 2: Check if the report was found
-        if (reportToChange != null) {
-            // Step 3: Display current status
-            System.out.println("Current Status: " + reportToChange.getStatus());
-
-            // Step 4: Update the status based on the user's button choice
-            switch (buttonChoice) {
-                case 1:
-                    reportToChange.setStatus("processing");
-                    System.out.println("Status updated to 'processing' successfully.");
-                    break;
-                case 2:
-                    reportToChange.setStatus("solved");
-                    System.out.println("Status updated to 'solved' successfully.");
-                    break;
-                default:
-                    System.out.println("Invalid button choice. Status not updated.");
-            }
-        } else {
-            System.out.println("Report not found.");
-        }
-    }
+   
 }
