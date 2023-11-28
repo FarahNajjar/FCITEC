d1a8ef5a0b108b08861f9fe11d8413f69c3a59d5
AishaAlajmi <2105522+AishaAlajmi@users.noreply.github.com>
2023-11-28 22:12:07 +0300

    Merge origin/master
    Conflicts:
    	
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- FCITEC/src/fcitec/User.java
+++ FCITEC/src/fcitec/User.java
@@ -62,7 +62,6 @@ public class User {
     }
   
     static void userInfo() {
-      
         System.out.print("Enter your name: ");
         String UserName =  FCITEC.in.next();
         System.out.print("Enter your Phone Number: ");
